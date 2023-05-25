f73b0e59652ac08d172677300be3b54f3afa359b
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2023-05-25 14:37:09 +0900

    HashMap

---
+++ HashMapEx5.java
@@ -0,0 +1,81 @@
+package ch11;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class HashMapEx5 {
+public static void main(String[] args) {
+	String[] data = {"A","K","A","K","D","K","A","K","K","K","Z","D"};
+		
+	TreeMap<Object, Object> map = new TreeMap<>();
+	
+	for(int i =0; i<data.length;i++) {
+		if(map.containsKey(data[i])) {
+		Integer value= (Integer)map.get(data[i]);
+		map.put(data[i], new Integer(value.intValue()+1));
+		}//if
+		else {
+			map.put(data[i], new Integer(1));
+		}
+	}//for
+	
+	
+	Iterator it = map.entrySet().iterator();
+	
+	while(it.hasNext()) {
+		Map.Entry entry = (Map.Entry)it.next();
+		int value = ((Integer)entry.getValue()).intValue();
+		System.out.println(entry.getKey() + " : " + printBar('#', value) + " " + value);
+	}
+	
+	Set<Entry<Object, Object>> set = map.entrySet();
+	List list = new ArrayList<>(set);
+	
+	Collections.sort(list, new ValueComparator());
+	
+	it = list.iterator();
+	
+	System.out.println("= 값의 크기가 큰 순서대로 =");
+	while(it.hasNext()) {
+		Map.Entry entry = (Map.Entry)it.next();
+		int value = ((Integer)entry.getValue()).intValue();
+		System.out.println(entry.getKey() + " : " + printBar('#', value) + " " + value);
+	}
+	
+	
+	
+	}//main
+
+static class ValueComparator implements Comparator {
+	public int compare(Object o1, Object o2) {
+		if(o1 instanceof Map.Entry && o2 instanceof Map.Entry) {
+			Map.Entry e1 = (Map.Entry)o1;
+			Map.Entry e2 = (Map.Entry)o2;
+			
+			int v1 = ((Integer)e1.getValue()).intValue();
+			int v2 = ((Integer)e2.getValue()).intValue();
+			
+			return v2 -v1;
+		}
+		return -1;
+	}
+	
+}
+public static String printBar(char ch, int value) {
+	char[] bar = new char[value];
+	
+	for(int i=0;i<bar.length;i++) bar[i] = ch;
+	
+	return new String(bar);	//String(char[] chArr)
+
+
+}
+}//class
